0681a2ce7601436b9f49500085ed87a5b2ffac60
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-06 17:59:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const bands = [
+    'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil',
+    'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State',
+    'We Came as Romans', 'Counterparts', 'Oh, Sleeper',
+    'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
+];
+
+const list = document.getElementById('band'); 
+
+bands.forEach((band) => {
+    const li = document.createElement('li');
+    li.textContent = band; 
+    list.appendChild(li); 
+});
